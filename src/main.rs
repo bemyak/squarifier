870e28f7271baf201fbcd17fec77401292a9a638
Sergej Gureev <sergej.gureev@relex.fi>
2020-09-13 17:12:49 +0300

    Imlement everythin planned

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use surf::url::Url;
-use tide::{Request, Response, Result as TideResult, StatusCode};
+use std::cmp::{max, min};
 
-const PAGE: &'static str = include_str!("site/index.html");
-const FAVICON: &'static [u8] = include_bytes!("site/favicon.ico");
+use image::{DynamicImage, ImageError, ImageFormat, RgbaImage};
+use mime::Name;
+use surf::{mime, url::Url};
+use tide::{Request, Response, StatusCode};
+
+const PAGE: &str = include_str!("site/index.html");
+const FAVICON: &[u8] = include_bytes!("site/favicon.ico");
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -22,68 +26,136 @@ async fn main() -> Result<(), std::io::Error> {
         res.set_body(FAVICON);
         Ok(res)
     });
-    app.at("/:param")
-        .get(|req: Request<()>| async move { process_req(req).await });
+    app.at("/:param").get(|req: Request<()>| async move {
+        let err = process_req(req).await;
+        match err {
+            Ok(r) => Ok(r),
+            Err(r) => Ok(r),
+        }
+    });
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
-async fn process_req(req: Request<()>) -> TideResult {
-    let param: Result<String, _> = req.param("param");
-    let res = match param {
-        Err(_) => {
-            let mut res = Response::new(StatusCode::BadRequest);
-            res.append_header("error", "You need to specify target url as parameter");
-            res
-        }
-        Ok(param) => {
-            let decoded = urldecode::decode(param);
-            let url = Url::parse(&decoded);
-            match url {
-                Err(err) => {
-                    let mut res = Response::new(StatusCode::BadRequest);
-                    let error = format!("Can't parse URL: {}", err.to_string());
-                    let error: &str = error.as_ref();
-                    res.append_header("error", error);
-                    res
-                }
-                Ok(url) => {
-                    let result = surf::get(url.clone()).await;
-                    match result {
-                        Err(err) => {
-                            let mut res = Response::new(StatusCode::InternalServerError);
-                            let error = format!("Can't fetch url {}: {}", url, err.to_string());
-                            let error: &str = error.as_ref();
-                            res.append_header("error", error);
-                            res
-                        }
-                        Ok(res1) => {
-                            // Response {
-                            //     response: Response {
-                            //         status: 200,
-                            //         version: HTTP/2.0,
-                            //         headers: {
-                            //             "content-type": "image/png",
-                            //             "last-modified": "Sun, 30 Aug 2020 08:28:48 GMT",
-                            //             "accept-ranges": "bytes",
-                            //             "etag": "\"8e2a4495a77ed61:0\"",
-                            //             "server": "Microsoft-IIS/10.0",
-                            //             "x-powered-by": "ASP.NET",
-                            //             "date": "Sat, 12 Sep 2020 17:13:00 GMT",
-                            //             "content-length": "341002",
-                            //         },
-                            //         body: Body {
-                            //             reader: "<hidden>",
-                            //         },
-                            //     },
-                            // }
-                            dbg!(res1);
-                            Response::new(StatusCode::Ok)
-                        }
-                    }
-                }
-            }
-        }
-    };
+type Err = Result<Response, Response>;
+
+async fn process_req(req: Request<()>) -> Err {
+    let param: String = req.param("param").map_err(|_| {
+        let mut res = Response::new(StatusCode::BadRequest);
+        res.append_header("error", "You need to specify target url as parameter");
+        res
+    })?;
+
+    let decoded = urldecode::decode(param);
+    let url = Url::parse(&decoded).map_err(|err| {
+        let mut res = Response::new(StatusCode::BadRequest);
+        let error = format!("Can't parse URL: {}", err.to_string());
+        let error: &str = error.as_ref();
+        res.append_header("error", error);
+        res
+    })?;
+
+    let mut res1 = surf::get(url.clone()).await.map_err(|err| {
+        let mut res = Response::new(StatusCode::InternalServerError);
+        let error = format!("Can't fetch url {}: {}", url, err.to_string());
+        let error: &str = error.as_ref();
+        res.append_header("error", error);
+        res
+    })?;
+
+    if !res1.status().is_success() {
+        let mut res = Response::new(res1.status().as_u16());
+        res.set_body(res1.body_bytes().await.unwrap_or_default());
+        return Err(res);
+    }
+
+    let mime = res1.mime().unwrap_or(mime::TEXT_PLAIN);
+    if mime.type_() != mime::IMAGE {
+        let mut res = Response::new(StatusCode::BadRequest);
+        let error = format!(
+            "Bad content type for {}, expected image, got: {}",
+            url, mime
+        );
+        let error: &str = error.as_ref();
+        res.append_header("error", error);
+        return Err(res);
+    }
+
+    let bytes = res1.body_bytes().await.map_err(|err| {
+        let mut res = Response::new(StatusCode::BadRequest);
+        let error = format!("Cannot read data from {}: {}", url, err);
+        let error: &str = error.as_ref();
+        res.append_header("error", error);
+        res
+    })?;
+
+    let format = mime_to_image_format(mime.subtype(), &url).map_err(|err| {
+        let mut res = Response::new(StatusCode::BadRequest);
+        let error = format!("Unsupported image type for {}: {}", url, err);
+        let error: &str = error.as_ref();
+        res.append_header("error", error);
+        res
+    })?;
+
+    let result = squarify(&bytes, format).map_err(|err| {
+        let mut res = Response::new(StatusCode::BadRequest);
+        let error = format!("Failed to decode image {}: {}", url, err);
+        let error: &str = error.as_ref();
+        res.append_header("error", error);
+        res
+    })?;
+
+    let mut res = Response::new(StatusCode::Ok);
+    res.set_body(result);
+    res.set_content_type(mime.to_string().as_ref());
     Ok(res)
+}
+
+fn mime_to_image_format(mime: Name, url: &Url) -> Result<ImageFormat, ImageError> {
+    match mime {
+        mime::PNG => Ok(ImageFormat::Png),
+        mime::JPEG => Ok(ImageFormat::Jpeg),
+        mime::GIF => Ok(ImageFormat::Gif),
+        mime::BMP => Ok(ImageFormat::Bmp),
+        _ => ImageFormat::from_path(url.to_string()),
+    }
+}
+
+fn squarify(bytes: &[u8], format: ImageFormat) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let src_image = image::load_from_memory_with_format(&bytes, format)?;
+    let src_rgba = src_image.to_rgba();
+
+    let mut min_x = src_rgba.width();
+    let mut max_x = 0;
+    let mut min_y = src_rgba.height();
+    let mut max_y = 0;
+
+    for (x, y, pixel) in src_rgba.enumerate_pixels() {
+        if pixel[3] != 0 {
+            max_x = max(x, max_x);
+            min_x = min(x, min_x);
+            max_y = max(y, max_y);
+            min_y = min(y, min_y);
+        }
+    }
+
+    let cropped_width = max_x - min_x;
+    let cropped_height = max_y - min_y;
+
+    let new_dimensions = max(cropped_width, cropped_height);
+    let shift_x = (new_dimensions - cropped_width) / 2;
+    let shift_y = (new_dimensions - cropped_height) / 2;
+
+    let mut new_image = RgbaImage::new(new_dimensions, new_dimensions);
+
+    for y in 0..cropped_height {
+        for x in 0..cropped_width {
+            let pixel = *src_rgba.get_pixel(x + min_x, y + min_y);
+            new_image.put_pixel(x + shift_x, y + shift_y, pixel);
+        }
+    }
+
+    let mut buff = Vec::new();
+    DynamicImage::ImageRgba8(new_image).write_to(&mut buff, format)?;
+    Ok(buff)
 }
